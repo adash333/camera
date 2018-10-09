324e7744228c10aaec5792270f08a37a9fe392a1
adash333 <adash333@hotmail.com>
2018-10-10 00:06:56 +0900

    second

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import { Plugins, CameraSource, CameraResultType } from '@capacitor/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  yourImage: any;
+
+  constructor() {
+
+  }
+
+  async captureImage() {
+    const capturedImage = await Plugins.Camera.getPhoto(
+      {
+        quality: 90,
+        allowEditing: true,
+        source: CameraSource.Camera,
+        resultType: CameraResultType.Base64,
+      }
+    );
+
+    this.yourImage = capturedImage.base64Data.slice(23);
+  }
 
 }
